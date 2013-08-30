c7b598abdcbf686916a64a241625f1e7ec67cb72
Matthew <matt@mattpeters.co.uk>
2013-08-30 09:04:03 -0700

    Added Shown event to base View

--- Cirrious/Cirrious.MvvmCross.SilverLight/Views/MvxSilverLightView.cs
+++ Cirrious/Cirrious.MvvmCross.SilverLight/Views/MvxSilverLightView.cs
@@ -5,6 +5,7 @@
 // 
 // Project Lead - Stuart Lodge, @slodge, me@slodge.com
 
+using System.Windows;
 using System.Windows.Controls;
 using Cirrious.MvvmCross.ViewModels;
 
@@ -20,7 +21,27 @@ namespace Cirrious.MvvmCross.SilverLight.Views {
 			}
 		}
 
-		public MvxSilverLightView() : base() { }
+		/// <summary>
+		/// This will only be called the very first time the control is shown.
+		/// If you use Loaded then it will be called each time the control is loaded on to the screen
+		/// </summary>
+		public event RoutedEventHandler Shown;
+		private bool hasBeenShown = false;
+
+
+		public MvxSilverLightView()
+			: base() {
+
+			this.Loaded += ( s, e ) => {
+				if ( hasBeenShown ) {
+					if ( Shown != null ) {
+						hasBeenShown = true; // this is here so that we will run it once after its set. and fail to run it if its set after the first time we're shown.
+						Shown.Invoke( e, e );
+					}
+				}
+			};
+
+		}
 	}
 
 	public class MvxSilverLightView<T> : MvxSilverLightView
